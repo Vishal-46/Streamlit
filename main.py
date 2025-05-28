edc73d0a1fe637cf754825beaa0a710b98f4557d
Vishal-46 <vishalmurugan006@gmail.com>
2025-05-28 05:58:55 +0530

    Commis

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import os
 st.write({"key":"Value"})
 st.write("Hello World")
 3+4
 pressed = st.button("press me")
 print(pressed)
+st.title("Stream lit workproject")
+st.header("Py workbook")
+st.subheader("Subheader")
+st.markdown("_Markdown_")
+st.caption("Small texts")
+code_ex = """ 
+def greet(name):
+    print("Welcome", name)
+"""
+st.code(code_ex, language="python")
+st.divider()
+
+st.image(os.path.join(os.getcwd(),"static","lz.jpg"))
